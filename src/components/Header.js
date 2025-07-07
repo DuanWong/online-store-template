e8ffacfc4840a114caa523e5c0379366e8355104
Duan <daniel.w2301@gmail.com>
2025-07-07 13:49:49 -0500

    responsive design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,43 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
+import { FiMenu, FiX } from 'react-icons/fi'; // 汉堡图标和关闭图标
 import logo from '../img/logo.png';
 import '../css/header.css';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+
     return (
         <header>
             <div className='header-container'>
                 <div className="header-logo">
                     <img src={logo} alt="Logo" />
                 </div>
-                <div>
-                    <nav>
-                        <ul className='header-list'>
-                            <li>
-                                <NavLink to="./" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/product' className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>
-                                    Product
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="./about" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
-                                    About us
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+                <nav className={`nav ${isOpen ? 'active' : ''}`}>
+                    <ul className='header-list'>
+                        <li>
+                            <NavLink to="./" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/product' className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>
+                                Product
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="./about" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
+                                About us
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <button className="hamburger" onClick={toggleMenu} aria-label="Toggle Menu">
+                    {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+                </button>
                 <div className="header-icons">
                     <div className="user-box">
                         <Link to="/user" >
